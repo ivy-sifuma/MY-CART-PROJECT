154fbc1b0e3f04acf15db26d3707271b79a35faf
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-05 23:27:35 +0300

    Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,14 @@ function Header({ isDarkMode, onDarkModeClick }) {
   return (
     <header>
       <h2>Shopperholics
-      <img src="https://e7.pngegg.com/pngimages/488/571/png-clipart-grocery-store-cost-food-expense-can-grocery-saving-food.png" alt="" width="70" height="40"></img>
+      <img src="https://e7.pngegg.com/pngimages/488/571/png-clipart-grocery-store-cost-food-expense-can-grocery-saving-food.png" alt="" width="70" height="70"></img>
       </h2>
       <a href="#home">Home 
-      <img src="https://png.pngtree.com/png-clipart/20190920/original/pngtree-green-juice-liquid-splash-illustration-png-image_4652693.jpg" alt="" width="70" height="40"></img>
+      <img src="https://png.pngtree.com/png-clipart/20190920/original/pngtree-green-juice-liquid-splash-illustration-png-image_4652693.jpg" alt="" width="70" height="70"></img>
       </a>
-      <a href="about us">About Us</a>
+      <a href="about us">About Us
+      <img src="https://thumbs.dreamstime.com/z/pay-credit-card-via-electronic-wallet-wirelessly-phone-new-mobile-banking-app-e-payment-vector-illustration-hand-193631712.jpg" alt="" width="60" height="70"></img>
+      </a>
       <button onClick={onDarkModeClick}>
         {isDarkMode ? "Dark" : "Light"} Mode
       </button>
