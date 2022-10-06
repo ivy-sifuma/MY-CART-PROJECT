e6fca2a311630d28be0e671569df609f302fcb08
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-06 11:14:27 +0300

    Cart

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer ({isDarkMode, onDarkModeClick}) {
+return (
+    <footer>
+        <h2>Get your fav produce! </h2>
+
+        <button onClick={onDarkModeClick}>
+        {isDarkMode ? "Dark" : "Light"} Mode
+      </button>
+    </footer>
+)
+
+
+}
+
+export default Footer;
