397bb54449f51d9a5a7d029f851925d317a79729
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-06 23:19:42 +0300

    Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 function Header({ isDarkMode, onDarkModeClick }) {
   return (
     <header>
-      <h2>Shopperholics
+      <h2>Shopperholics <br></br>
       <img src="https://e7.pngegg.com/pngimages/488/571/png-clipart-grocery-store-cost-food-expense-can-grocery-saving-food.png" alt="" width="70" height="70"></img>
       </h2>
       <a href="#home">Home </a>
