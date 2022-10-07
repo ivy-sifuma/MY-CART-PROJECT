92b7f0ec72dc886c82b0fcd30cc310d4b0ea85fb
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-07 10:12:51 +0300

    Cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ function Header({ isDarkMode, onDarkModeClick }) {
       <h2>Shopperholics <br></br>
       <img src="https://e7.pngegg.com/pngimages/488/571/png-clipart-grocery-store-cost-food-expense-can-grocery-saving-food.png" alt="" width="70" height="70"></img>
       </h2>
-      
+
+      <a href="Home"></a>
+    <br>
+    <a href="About"></a>
+      </br>
       <button onClick={onDarkModeClick}>
         {isDarkMode ? "Dark" : "Light"} Mode
       </button>
